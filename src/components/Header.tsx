ea9fb68baca07e3277d3a12e912dc07ffafd238e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-15 14:28:52 +0100

    bootstrapped project and implemented header

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        <Link href="/" className="font-bold text-xl">
+          HireNet
+        </Link>
+
+        <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
+          <Link href="/login" className="bg-gray-200 ">
+            Login
+          </Link>
+          <Link href="/newlisting" className="bg-blue-600 text-white">
+            Post a job
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
