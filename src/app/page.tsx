01279370abe79a6821275fce25e9f52160f79264
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-17 09:57:49 +0100

    implemented authentication with workos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 import Hero from "@/components/Hero";
 import Jobs from "@/components/Jobs";
-import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
+
   return (
     <div>
       <Hero />
       <Jobs />
     </div>
   );
-}
+};
+export default Home;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,56 @@
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
 
-const Header = () => {
+async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
+  const signOutUrl = await getSignUpUrl();
+  console.log(user);
+
   return (
     <header className="">
+      {/* {JSON.stringify(user)} */}
       <div className="container mx-auto flex justify-between items-center my-4">
         <Link href="/" className="font-bold text-xl">
           HireNet
         </Link>
 
-        <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
-          <Link href="/login" className="bg-gray-200 ">
-            Login
-          </Link>
-          <Link href="/newlisting" className="bg-blue-600 text-white">
+        <nav className="flex gap-2">
+          {!user ? (
+            <Link href={signInUrl} className="bg-gray-200 py-2 px-4 rounded-md">
+              Login
+            </Link>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-gray-200 py-2 px-4 rounded-md"
+              >
+                Logout, {user.firstName}
+              </button>
+            </form>
+          )}
+
+          <Link
+            href="/newlisting"
+            className="bg-blue-600 text-white  py-2 px-4 rounded-md"
+          >
             Post a job
           </Link>
         </nav>
       </div>
     </header>
   );
-};
+}
 
 export default Header;
